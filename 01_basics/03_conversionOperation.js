0c97abd3bbf978e061f1f4c0fab3127bb9c6e101
Omyadav7354 <omyadav7354@gmail.com>
2024-07-15 13:20:21 +0000

    conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,3 +46,43 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber); // string
 console.log(stringNumber); //33
+
+
+// ************************** Operations **************************
+
+
+
+let value = 3
+let negValue = -value
+console.log(negValue); // -3
+
+// console.log(2+2);// 4
+// console.log(2-2);// 0
+// console.log(2*2);// 4 
+// console.log(2**3);// 8
+// console.log(2/3);// 0.66666666666666
+// console.log(2%3);// 2
+
+let str1 = "hello"
+let str2 = " op"
+
+let str3 = str1 + str2
+console.log(str3);  // hello op
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+
+
+console.log(+true); // 1
+// console.log(true+); // error
+console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // code must be easy to understand
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // 101  & ++gameCounter => 101
