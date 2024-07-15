7f25c8dced7db38f1bba28b2e9ccf249efc6a907
Omyadav7354 <omyadav7354@gmail.com>
2024-07-15 09:53:33 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+// alert("3+3") // we are using node JS not browser
+
+console.log(3
+    +3) // code readability that should be high
+
+console.log("OmPrakash")
+
+let name = "OmPrakash"
+let age = 22
+let isLoggedIn = false
+let state;
+let temprature = null
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//symbole => unique
+
+
+
+// object
+
+
+console.log(typeof "OmPrakash"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object- this is error in java scrict
+console.log(typeof undefined); // undefined
